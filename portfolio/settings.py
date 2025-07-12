edf164ec933f45e60a1859d9854a66e862cf9c95
abhichavan32 <163678210+abhichavan32@users.noreply.github.com>
2025-07-12 18:01:39 +0530

    Configure static files for Render with WhiteNoise

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,5 +1,7 @@
 import os  # Add this at the top of the file
 
+
+
 """
 Django settings for portfolio project.
 
@@ -16,6 +18,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -53,6 +58,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ← Add this
+    ...
+]
+
 
 ROOT_URLCONF = 'portfolio.urls'
 
